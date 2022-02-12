816b13dbf8bb176f0f08d0503e7b8991e0d7556b
codewdy <codewdy@gmail.com>
2022-02-12 14:25:11 +0800

    add implement for lightning chain

--- skills/lightning_chain.lua
+++ skills/lightning_chain.lua
@@ -3,16 +3,29 @@ local M = {}
 local effect_manager = require("gamesystem.effect_manager")
 local effects = require("effects")
 local unit_manager = require("gamesystem.unit_manager")
+local projectile = require("skills.utils.projectile")
+
+function M.shoot(context, src, dest)
+  effect_manager.add_effect(effects.lightning,
+      src.posX, src.posY, dest.posX, dest.posY, 12)
+  return 3
+end
+
+function M.hit(context, src, dest)
+  dest.hp = dest.hp - 0
+end
 
 function M.cast(arg, unit, target)
-  local src = unit
-  local dest = target
-  for i=1,10 do
-    effect_manager.add_effect(effects.lightning,
-    src.posX, src.posY, dest.posX, dest.posY, 10)
-    src = dest
-    dest = unit_manager.random_nearby_unit(src, target.group, 5, false)
-  end
+  local context = projectile.create()
+  context.shoot = M.shoot
+  context.hit = M.hit
+  context.source = unit
+  context.target = target
+  -- Mock
+  context.target = unit_manager.random_unit(2)
+  context.arg = arg
+  context.count = 5
+  context.cast(context)
 end
 
 return M
