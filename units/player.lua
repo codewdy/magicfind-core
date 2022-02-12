85df8904e33164e8cb3cbf942b69857d8a750908
codewdy <codewdy@gmail.com>
2022-02-12 14:25:23 +0800

    add mock skills for player

--- units/player.lua
+++ units/player.lua
@@ -9,13 +9,15 @@ M.prototype.size = 2
 
 local lightning_chain = require("skills.lightning_chain")
 local skill_arg = require("gamesystem.skill_arg")
-M.prototype.skills.size = 1
-M.prototype.skills.vec[1] = skill_arg.create({
+M.prototype.skills.size = 10
+for i=1,M.prototype.skills.size do
+M.prototype.skills.vec[i] = skill_arg.create({
   type = skill_arg.TIMEOUT,
   skill = lightning_chain,
-  factor = 0.02,
-  id = 1
+  factor = 1,
+  id = i
 })
+end
 
 player_manager.set_prototype(M.prototype)
 
